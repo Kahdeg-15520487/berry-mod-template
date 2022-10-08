b416212330873883b7976eabbe77a26316c08b98
Nguyễn Lê Minh <minhnguyenle1997@gmail.com>
2022-10-08 13:53:36 +0700

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,4 +1,4 @@
-New-Item -Path "TSMMPackage/plugins" -ItemType Directory -Force
+$o = New-Item -Path "TSMMPackage/plugins" -ItemType Directory -Force
 Copy-Item "icon.png" -Destination "./TSMMPackage" -Force
 Copy-Item "README.md" -Destination "./TSMMPackage" -Force
 Copy-Item "manifest.json" -Destination "./TSMMPackage" -Force
@@ -8,16 +8,22 @@ Copy-Item "./Images" -Destination "./TSMMPackage/plugins" -Recurse -Force
 Copy-Item "./Sounds" -Destination "./TSMMPackage/plugins" -Recurse -Force
 
 $buildResult = [string[]](dotnet build .)
+$buildResult = [string[]](dotnet build .)
+if ($LASTEXITCODE -eq 0){
+	Write-Host "compile success"
+}
 $dll = $buildResult[5].Split('>')[1].Trim()
 Copy-Item $dll -Destination "./TSMMPackage/plugins" -Force
 
+Write-Host "generating thunderstore mod package"
 Compress-Archive -Path "./TSMMPackage/*" -DestinationPath "TSMMPackage.zip" -Force
 
 $pluginsPath = "$env:APPDATA\Thunderstore Mod Manager\DataFolder\Stacklands\profiles\Default\BepInEx\plugins"
 $pluginManifest = Get-Content -Raw .\manifest.json | ConvertFrom-Json
 $pluginFolderName = "local-$($pluginManifest.name)"
 $pluginFolder = "$pluginsPath\$pluginFolderName"
-New-Item -Path "$pluginFolder" -ItemType Directory -Force
+$o = New-Item -Path "$pluginFolder" -ItemType Directory -Force
+Write-Host "syncing folder"
 Copy-Item "icon.png" -Destination "$pluginFolder" -Force
 Copy-Item "README.md" -Destination "$pluginFolder" -Force
 Copy-Item "manifest.json" -Destination "$pluginFolder" -Force
